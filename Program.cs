8688c404faae92eee3d4e366cd2ddae1479b738a
JuliaNakano <jumartinsnakano@gmail.com>
2021-09-19 18:34:39 -0300

    Solução do exercício Juros Simples

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercicio_JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal montante;
+            double taxajuros, capital, juros, tempo;
+        
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("\nApós digitar todas as informaões necessárias  o progama ira calcular o montante final de um investimento a juros simples.\n");
+            Console.ResetColor();
+            
+            Console.Write("Digite o seu capital:...... R$ ");
+            capital = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a taxa de juros:......% ");
+            taxajuros = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o tempo (meses):........");
+            tempo = Convert.ToDouble(Console.ReadLine());
+
+            juros= capital * (taxajuros/100) * tempo;
+            montante = Convert.ToDecimal(juros + capital);
+
+            Console.WriteLine();
+            Console.WriteLine($"Juros:.....R${juros},00");
+            Console.WriteLine($"Montante:..R${montante},00");
+        }
+    }
+}
