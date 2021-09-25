98a38aa263026b76eff9609ef45ced25901bb32a
JuliaNakano <jumartinsnakano@gmail.com>
2021-09-24 22:28:05 -0300

    correção da solução do exercício juros simples

--- Program.cs
+++ Program.cs
@@ -7,27 +7,28 @@ namespace Exercicio_JurosSimples
         static void Main(string[] args)
         {
             decimal montante;
-            double taxajuros, capital, juros, tempo;
+            decimal taxajuros, capital, juros, tempo;
         
             Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.WriteLine("\nApós digitar todas as informaões necessárias  o progama ira calcular o montante final de um investimento a juros simples.\n");
             Console.ResetColor();
             
             Console.Write("Digite o seu capital:...... R$ ");
-            capital = Convert.ToDouble(Console.ReadLine());
+            capital = Convert.ToDecimal(Console.ReadLine());
+            
 
             Console.Write("Digite a taxa de juros:......% ");
-            taxajuros = Convert.ToDouble(Console.ReadLine());
+            taxajuros = Convert.ToDecimal(Console.ReadLine());
 
             Console.Write("Digite o tempo (meses):........");
-            tempo = Convert.ToDouble(Console.ReadLine());
+            tempo = Convert.ToDecimal(Console.ReadLine());
 
             juros= capital * (taxajuros/100) * tempo;
             montante = Convert.ToDecimal(juros + capital);
 
             Console.WriteLine();
-            Console.WriteLine($"Juros:.....R${juros},00");
-            Console.WriteLine($"Montante:..R${montante},00");
+            Console.WriteLine($"Juros:.....R${juros}");
+            Console.WriteLine($"Montante:..R${montante}");
         }
     }
 }
